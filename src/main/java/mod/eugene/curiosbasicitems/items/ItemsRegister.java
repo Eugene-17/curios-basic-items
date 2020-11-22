62d86199def60c7063b856bdbb034a2349d41ecf
BinhDV <dangbinh1996914@gmail.com>
2020-11-22 09:21:13 +0700

    change how health amulet work

--- src/main/java/mod/eugene/curiosbasicitems/items/ItemsRegister.java
+++ src/main/java/mod/eugene/curiosbasicitems/items/ItemsRegister.java
@@ -1,14 +1,16 @@
 package mod.eugene.curiosbasicitems.items;
+
 import mod.eugene.curiosbasicitems.CuriosBasicItems;
 import nerdhub.cardinal.components.api.event.ItemComponentCallbackV2;
-import com.google.common.collect.HashMultimap;
-import com.google.common.collect.Multimap;
-import top.theillusivec4.curios.api.CuriosApi;
+
+import java.util.UUID;
+
 import top.theillusivec4.curios.api.CuriosComponent;
 import top.theillusivec4.curios.api.type.component.ICurio;
-import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeInstance;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
 import net.minecraft.entity.attribute.EntityAttributeModifier.Operation;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.sound.SoundCategory;
@@ -20,7 +22,18 @@ public class ItemsRegister {
     public static final IronHealthAmulet IRON_HEALTH_AMULET = new IronHealthAmulet();
 	public static final NetheriteHealthAmulet NETHERITE_HEALTH_AMULET = new NetheriteHealthAmulet();
     public static final StarHealthAmulet STAR_HEALTH_AMULET = new StarHealthAmulet();
-    
+
+    private static final void resetHealth(UUID UUID_, LivingEntity livingEntity){
+        if (livingEntity instanceof PlayerEntity) {
+            EntityAttributeModifier modifier = new EntityAttributeModifier(
+                UUID_, "Health bonus", 0, Operation.ADDITION);
+            EntityAttributeInstance maxHealthAttr = livingEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+            maxHealthAttr.removeModifier(modifier);
+            maxHealthAttr.addPersistentModifier(modifier);
+        }
+        if (livingEntity.getHealth() > 20) livingEntity.setHealth(livingEntity.getMaxHealth());
+    }
+
     public static void register() {
         //Register new item
         Registry.register(Registry.ITEM, new Identifier(CuriosBasicItems.MODID, "iron_health_amulet"), IRON_HEALTH_AMULET);
@@ -31,22 +44,23 @@ public class ItemsRegister {
 		ItemComponentCallbackV2.event(IRON_HEALTH_AMULET).register(
             ((item, itemStack, componentContainer) -> componentContainer
                 .put(CuriosComponent.ITEM, new ICurio() {
-    
+
                     @Override
-                    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(
-                        String identifier) {
-                          Multimap<EntityAttribute, EntityAttributeModifier> attributes = HashMultimap
-                              .create();
-      
-                          if (CuriosApi.getCuriosHelper().getCurioTags(itemStack.getItem())
-                              .contains(identifier)) {
-                        attributes.put(EntityAttributes.GENERIC_MAX_HEALTH,
-                            new EntityAttributeModifier(IronHealthAmulet.HEALTH_UUID, "Health bonus",
-                                                        4, Operation.ADDITION));
-                          }
-                      return attributes;
+					public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+						if (livingEntity instanceof PlayerEntity) {
+                            EntityAttributeModifier modifier = new EntityAttributeModifier(
+                                    IronHealthAmulet.HEALTH_UUID, "Health bonus", 4, Operation.ADDITION);
+                            EntityAttributeInstance maxHealthAttr = livingEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                            maxHealthAttr.removeModifier(modifier);
+                            maxHealthAttr.addPersistentModifier(modifier);
+						}
                     }
-    
+                    
+                    @Override
+					public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+						resetHealth(IronHealthAmulet.HEALTH_UUID, livingEntity);
+                    }
+
                     @Override
                     public void playRightClickEquipSound(LivingEntity livingEntity) {
                           livingEntity.world
@@ -63,22 +77,23 @@ public class ItemsRegister {
             ItemComponentCallbackV2.event(NETHERITE_HEALTH_AMULET).register(
             ((item, itemStack, componentContainer) -> componentContainer
                 .put(CuriosComponent.ITEM, new ICurio() {
-    
+
                     @Override
-                    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(
-                        String identifier) {
-                          Multimap<EntityAttribute, EntityAttributeModifier> attributes = HashMultimap
-                              .create();
-      
-                          if (CuriosApi.getCuriosHelper().getCurioTags(itemStack.getItem())
-                              .contains(identifier)) {
-                        attributes.put(EntityAttributes.GENERIC_MAX_HEALTH,
-                            new EntityAttributeModifier(NetheriteHealthAmulet.HEALTH_UUID, "Health bonus",
-                                                        10, Operation.ADDITION));
-                          }
-                      return attributes;
+					public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+						if (livingEntity instanceof PlayerEntity) {
+                            EntityAttributeModifier modifier = new EntityAttributeModifier(
+                                NetheriteHealthAmulet.HEALTH_UUID, "Health bonus", 10, Operation.ADDITION);
+                            EntityAttributeInstance maxHealthAttr = livingEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                            maxHealthAttr.removeModifier(modifier);
+                            maxHealthAttr.addPersistentModifier(modifier);
+						}
                     }
-    
+                    
+                    @Override
+					public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+						resetHealth(NetheriteHealthAmulet.HEALTH_UUID, livingEntity);
+                    }
+
                     @Override
                     public void playRightClickEquipSound(LivingEntity livingEntity) {
                           livingEntity.world
@@ -97,18 +112,19 @@ public class ItemsRegister {
                 .put(CuriosComponent.ITEM, new ICurio() {
     
                     @Override
-                    public Multimap<EntityAttribute, EntityAttributeModifier> getAttributeModifiers(
-                        String identifier) {
-                          Multimap<EntityAttribute, EntityAttributeModifier> attributes = HashMultimap
-                              .create();
-      
-                          if (CuriosApi.getCuriosHelper().getCurioTags(itemStack.getItem())
-                              .contains(identifier)) {
-                        attributes.put(EntityAttributes.GENERIC_MAX_HEALTH,
-                            new EntityAttributeModifier(NetheriteHealthAmulet.HEALTH_UUID, "Health bonus",
-                                                        20, Operation.ADDITION));
-                          }
-                      return attributes;
+					public void onEquip(String identifier, int index, LivingEntity livingEntity) {
+						if (livingEntity instanceof PlayerEntity) {
+                            EntityAttributeModifier modifier = new EntityAttributeModifier(
+                                StarHealthAmulet.HEALTH_UUID, "Health bonus", 20, Operation.ADDITION);
+                            EntityAttributeInstance maxHealthAttr = livingEntity.getAttributeInstance(EntityAttributes.GENERIC_MAX_HEALTH);
+                            maxHealthAttr.removeModifier(modifier);
+                            maxHealthAttr.addPersistentModifier(modifier);
+						}
+                    }
+                    
+                    @Override
+					public void onUnequip(String identifier, int index, LivingEntity livingEntity) {
+						resetHealth(StarHealthAmulet.HEALTH_UUID, livingEntity);
                     }
     
                     @Override
