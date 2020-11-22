9d2846efccd7f88ece6d9b0801da1966ef529990
BinhDV <dangbinh1996914@gmail.com>
2020-11-22 11:49:58 +0700

    add tooltip to amulet

--- src/main/java/mod/eugene/curiosbasicitems/items/IronHealthAmulet.java
+++ src/main/java/mod/eugene/curiosbasicitems/items/IronHealthAmulet.java
@@ -1,14 +1,24 @@
 package mod.eugene.curiosbasicitems.items;
 
+import java.util.List;
 import java.util.UUID;
-
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 
 public class IronHealthAmulet extends Item {
     public static final UUID HEALTH_UUID = UUID.fromString("7119a370-1fc4-4cb3-b7e3-4747788f0785");
 
     public IronHealthAmulet() {
         super(new Item.Settings().group(ItemGroup.TOOLS).maxCount(1).maxDamage(2000));
+    }
+    
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.curiosbasicitems.iron_health_amulet.tooltip"));
     }
 }   
--- src/main/java/mod/eugene/curiosbasicitems/items/StarHealthAmulet.java
+++ src/main/java/mod/eugene/curiosbasicitems/items/StarHealthAmulet.java
@@ -1,16 +1,25 @@
 package mod.eugene.curiosbasicitems.items;
 
+import java.util.List;
 import java.util.UUID;
-
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.TranslatableText;
+import net.minecraft.text.Text;
+import net.minecraft.world.World;
 
 public class StarHealthAmulet extends Item {
     public static final UUID HEALTH_UUID = UUID.fromString("66bbc8c0-def8-404d-9687-eb39a6be5927");
 
     public StarHealthAmulet() {
         super(new Item.Settings().group(ItemGroup.TOOLS).maxCount(1).maxDamage(2000));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack itemStack, World world, List<Text> tooltip, TooltipContext tooltipContext) {
+        tooltip.add(new TranslatableText("item.curiosbasicitems.star_health_amulet.tooltip"));
     }
 
     @Override
