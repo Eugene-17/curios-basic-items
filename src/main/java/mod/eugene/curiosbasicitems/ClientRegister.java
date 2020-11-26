015d6d3645d1e57927ca9debb0af02963fe2eb36
BinhDV <dangbinh1996914@gmail.com>
2020-11-26 14:12:24 +0700

    add more slots

--- src/main/java/mod/eugene/curiosbasicitems/ClientRegister.java
+++ src/main/java/mod/eugene/curiosbasicitems/ClientRegister.java
@@ -1,13 +1,16 @@
 package mod.eugene.curiosbasicitems;
-
+import mod.eugene.curiosbasicitems.CuriosBasicItems;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
 import net.fabricmc.fabric.api.network.ClientSidePacketRegistry;
 import net.minecraft.client.options.KeyBinding;
 import org.lwjgl.glfw.GLFW;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.PacketByteBuf;
+import net.minecraft.screen.PlayerScreenHandler;
+import net.minecraft.util.Identifier;
 import io.netty.buffer.Unpooled;
 import mod.eugene.curiosbasicitems.models.ModelRegister;
 
@@ -31,5 +34,12 @@ public class ClientRegister implements ClientModInitializer {
         }));
 
         ModelRegister.register();
+        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE)
+        .register(((spriteAtlasTexture, registry) -> {
+            registry.register(new Identifier(CuriosBasicItems.MODID, "item/empty_mask_slot"));
+            registry.register(new Identifier(CuriosBasicItems.MODID, "item/empty_left_belt_slot"));
+            registry.register(new Identifier(CuriosBasicItems.MODID, "item/empty_right_belt_slot"));
+        }));
+        
     }
 }
--- src/main/java/mod/eugene/curiosbasicitems/CuriosBasicItems.java
+++ src/main/java/mod/eugene/curiosbasicitems/CuriosBasicItems.java
@@ -14,12 +14,16 @@ import mod.eugene.curiosbasicitems.items.CuriosItemGroup;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import net.fabricmc.fabric.api.tag.TagRegistry;
+import top.theillusivec4.curios.api.SlotTypeInfo.Builder;
 
 public class CuriosBasicItems implements ModInitializer {
 
 	public static final String MODID = "curiosbasicitems";
 	public static final Logger LOGGER = LogManager.getLogger();
 	public static Tag<Item> PIGLIN_SAFE_CURIOS;
+	public static Builder MASK_BUILDER = new Builder("mask").priority(1001).icon(new Identifier(MODID, "item/empty_mask_slot"));
+	public static Builder LEFT_BELT_BUILDER = new Builder("left_belt").priority(1002).icon(new Identifier(MODID, "item/empty_left_belt_slot"));
+	public static Builder RIGHT_BELT_BUILDER = new Builder("right_belt").priority(1003).icon(new Identifier(MODID, "item/empty_right_belt_slot"));
 	
 	@Override
 	public void onInitialize() {
@@ -31,8 +35,11 @@ public class CuriosBasicItems implements ModInitializer {
 		CuriosApi.enqueueSlotType(BuildScheme.REGISTER, SlotTypePreset.BACK.getInfoBuilder().build());
         CuriosApi.enqueueSlotType(BuildScheme.REGISTER, SlotTypePreset.CHARM.getInfoBuilder().build());
         CuriosApi.enqueueSlotType(BuildScheme.REGISTER, SlotTypePreset.BRACELET.getInfoBuilder().build());
-        CuriosApi.enqueueSlotType(BuildScheme.REGISTER, SlotTypePreset.BELT.getInfoBuilder().build());
-		
+		CuriosApi.enqueueSlotType(BuildScheme.REGISTER, SlotTypePreset.BELT.getInfoBuilder().build());
+		CuriosApi.enqueueSlotType(BuildScheme.REGISTER, MASK_BUILDER.build());
+		CuriosApi.enqueueSlotType(BuildScheme.REGISTER, LEFT_BELT_BUILDER.build());
+		CuriosApi.enqueueSlotType(BuildScheme.REGISTER, RIGHT_BELT_BUILDER.build());
+
 		PIGLIN_SAFE_CURIOS = TagRegistry.item(new Identifier(MODID, "piglin_safe_curios"));
 
 		AmuletsRegister.register();
